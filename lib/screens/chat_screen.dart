6efa6674a603376317c2a0d6b3bdf6fd8fe41674
Leman316 <56719469+Leman316@users.noreply.github.com>
2021-01-19 18:04:46 +0600

    Update 1.1

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,26 +5,31 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/K6LuRtFubAJHpWmFwHDL/messages')
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((element) {
-              print(element['text']);
-            });
-          });
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chats/K6LuRtFubAJHpWmFwHDL/messages')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final documents = snapshot.data.docs;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(documents[index]['text']),
+            ),
+          );
         },
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('chats/K6LuRtFubAJHpWmFwHDL/messages')
+                .add({'text': 'Ohhhh, Elden Ring'});
+          }),
     );
   }
 }

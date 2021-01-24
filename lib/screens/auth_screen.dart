33387006f453a3d2e8444e83621c2ccd09e57def
Leman316 <56719469+Leman316@users.noreply.github.com>
2021-01-24 14:24:40 +0600

    Update 1.5

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,4 @@
 import 'package:chat_app/widgets/auth_form.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
